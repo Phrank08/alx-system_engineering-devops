29a48355ed9fc84733e38a3820b0c1528c1afad6
Phrank08 <frankokey469@gmail.com>
2024-07-05 13:45:09 -0700

    creates a file using Puppet with permissions

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creates a new file with permissions
+
+file { '/tmp/school':
+  ensure	=>  present,
+  content	=>  'I love Puppet',
+  mode		=>  '0744',
+  owner		=>  'www-data',
+  group		=>  'www-data',
+ }
