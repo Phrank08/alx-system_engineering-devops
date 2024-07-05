0f5f5fa45a19595839b1c3929781046b8d438586
Phrank08 <frankokey469@gmail.com>
2024-07-05 14:03:55 -0700

    add: Fix all errors

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,7 @@
-# Creates a new file with permissions
-
+# create file /tmp/school
 file { '/tmp/school':
-  ensure	=>  present,
-  content	=>  'I love Puppet',
-  mode		=>  '0744',
-  owner		=>  'www-data',
-  group		=>  'www-data',
- }
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet'
+}
