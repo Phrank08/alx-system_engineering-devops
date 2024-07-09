77da617d3c36089a83acab0bace92267cdecfd8c
Phrank08 <frankokey469@gmail.com>
2024-07-09 01:51:46 -0700

    Task Done

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,10 @@
-# create file /tmp/school
-file { '/tmp/school':
-	mode    => '0744',
-	owner   => 'www-data',
-	group   => 'www-data',
-	content => 'I love Puppet'
+# creating a file in /tmp  called school 
+$file_path = '/tmp/school'
+
+file { $file_path:
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
